5431a51d6b209efaf2accc216284e68f52378b37
Kazuki <i.kazu861@gmail.com>
2020-02-15 17:37:34 +0900

    DB修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,3 @@ class Like < ApplicationRecord
     validates :user_id, {presence: true}
     validates :post_id, {presence: true}
   end
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users,controllers: {
     registrations: 'users/registrations',
   }
+  
   root "home#top"
   get "about" => "home#about"
 
